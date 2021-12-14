8cd8f9c7956dd541fb7b99dcb4f0a745ecfb9a60
mrumenov-cyber <marija.rumenovic@fesb.hr>
2021-12-14 22:11:19 +0100

    Movies added to the main-view
    My Movies added to the main-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -10,9 +10,20 @@ export class MainView extends React.Component {
     super();
     this.state = {
       movies: [
-        { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...'},
-        { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
-        { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'}
+        { _id: 1, 
+          Title: "Harry Potter and the Prisioner of Azkaban",
+        Description: "The movie follows Harry Potter into his third year at Hogwarts. Along with his friends Ron and Hermione, Harry investigates Sirius Black, an escaped prisioner from Azkaban, the wizard prision, believed to be one of Lord Voldemort's old allies.",
+        ImagePath: "https://m.media-amazon.com/images/M/MV5BMTY4NTIwODg0N15BMl5BanBnXkFtZTcwOTc0MjEzMw@@._V1_FMjpg_UX1000_.jpg"},
+
+        { _id: 2, 
+          Title: "Harry Potter and the Order of Phoneix",
+          Description: "The Order of the Phoenix is a secret organisation in the Harry Potter series of fiction books written by J. K. Rowling. Founded by Albus Dumbledore to fight Lord Voldemort and his followers, the Death Eaters, the Order lends its name to the fifth book of the series, Harry Potter and the Order of the Phoenix.",
+          ImagePath: "https://m.media-amazon.com/images/M/MV5BMTI1NDMyMjExOF5BMl5BanBnXkFtZTcwOTc4MjQzMQ@@._V1_QL75_UY207_CR0,0,140,207_.jpg"},
+        
+        { _id: 3, 
+          Title: "Harry Potter and Sorcerer's Stone",
+          Description: "Start of Harry Potter schooling and Wizard journey. His very first year at Hogwards, the school of wizards. The red stone in the movie had the power to create the Elixir of Life, a potion that would make someone drinking it immortal.",
+          ImagePath: "https://m.media-amazon.com/images/M/MV5BNjQ3NWNlNmQtMTE5ZS00MDdmLTlkZjUtZTBlM2UxMGFiMTU3XkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_.jpg"}
       ],
       selectedMovie:null
     }
